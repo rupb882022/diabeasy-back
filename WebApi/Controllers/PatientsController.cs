631da7ea1dbd110aaa51c98de2cbfca34b1e82e2
tal farkash <tal_farkash@walla.com>
2022-02-23 14:19:33 +0200

    add login

--- WebApi/Controllers/PatientsController.cs
+++ WebApi/Controllers/PatientsController.cs
@@ -23,7 +23,7 @@ namespace WebApi.Controllers
                         tblPatients patient = DB.tblPatients.Where(x => x.id == id).SingleOrDefault();
                         if (patient == null)
                         {
-                            return null;
+                            return Content(HttpStatusCode.OK, assisant_phone);
                         }
                         assisant_phone = (int)patient.assistant_phone;
                         break;
@@ -32,7 +32,7 @@ namespace WebApi.Controllers
                         return null;
                 }
                 //List<string> s = DB.tblPatients.Select(x => x.firstname + " " + x.lastname + "  email:" + x.email).ToList();
-                return Content(HttpStatusCode.OK, assisant_phone);
+                return null;
             }
             catch (Exception e)
             {
@@ -42,6 +42,39 @@ namespace WebApi.Controllers
 
         }
 
- 
+        public IHttpActionResult Get(string url, string email, string password)
+        {
+
+            try
+            {
+                switch (url)
+                {
+                    case "userDetails":
+                        tblPatients user = DB.tblPatients.Where(x => x.email == email && x.password == password).SingleOrDefault();
+                        if (user != null)
+                        {
+                            var userDetalis = new { id = user.id, profileimage = user.profileimage, name = user.firstname + " " + user.lastname };
+                            return Content(HttpStatusCode.OK, userDetalis);
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+
+                    default:
+                        break;  
+                }
+                return null;
+
+            }
+            catch (Exception e)
+            {
+
+                return Content(HttpStatusCode.BadRequest, e.Message);
+            }
+
+        }
+
+
     }
 }
