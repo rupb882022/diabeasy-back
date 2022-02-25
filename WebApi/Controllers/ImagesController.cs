e0211e2cfe0a941b9ea8b4da369e14ff3e8065e4
idanlavee <idanlavee10@gmail.com>
2022-02-25 15:15:07 +0200

    post image try1

---
+++ WebApi/Controllers/ImagesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Cors;
+using diabeasy_back;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
+    public class ImagesController : ApiController
+    {
+     diabeasyDBContext DB = new diabeasyDBContext();
+        //just for trying
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            try
+            {
+                List<string> s = DB.tblPatients.Select(x => x.firstname + " " + x.lastname + "  email:" + x.email).ToList();
+                //return Content(HttpStatusCode.OK,s);
+                return s;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        [Route("uploadpicture")]
+        public Task<HttpResponseMessage> Post()
+        {
+            string outputForNir = "start---";
+            List<string> savedFilePath = new List<string>();
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            //Where to put the picture on server  ...MapPath("~/TargetDir")
+            string rootPath = HttpContext.Current.Server.MapPath("~/uploadFiles");
+            var provider = new MultipartFileStreamProvider(rootPath);
+            var task = Request.Content.ReadAsMultipartAsync(provider).
+                ContinueWith<HttpResponseMessage>(t =>
+                {
+                    if (t.IsCanceled || t.IsFaulted)
+                    {
+                        Request.CreateErrorResponse(HttpStatusCode.InternalServerError, t.Exception);
+                    }
+                    foreach (MultipartFileData item in provider.FileData)
+                    {
+                        try
+                        {
+                            outputForNir += " ---here";
+                            string name = item.Headers.ContentDisposition.FileName.Replace("\"", "");
+                            outputForNir += " ---here2=" + name;
+
+                            //need the guid because in react native in order to refresh an inamge it has to have a new name
+                            string newFileName = Path.GetFileNameWithoutExtension(name) + "_" + CreateDateTimeWithValidChars() + Path.GetExtension(name);
+                            //string newFileName = Path.GetFileNameWithoutExtension(name) + "_" + Guid.NewGuid() + Path.GetExtension(name);
+                            //string newFileName = name + "" + Guid.NewGuid();
+                            outputForNir += " ---here3" + newFileName;
+
+                            //delete all files begining with the same name
+                            string[] names = Directory.GetFiles(rootPath);
+                            foreach (var fileName in names)
+                            {
+                                if (Path.GetFileNameWithoutExtension(fileName).IndexOf(Path.GetFileNameWithoutExtension(name)) != -1)
+                                {
+                                    File.Delete(fileName);
+                                }
+                            }
+
+                            //File.Move(item.LocalFileName, Path.Combine(rootPath, newFileName));
+                            File.Copy(item.LocalFileName, Path.Combine(rootPath, newFileName), true);
+                            File.Delete(item.LocalFileName);
+                            outputForNir += " ---here4";
+
+                            Uri baseuri = new Uri(Request.RequestUri.AbsoluteUri.Replace(Request.RequestUri.PathAndQuery, string.Empty));
+                            outputForNir += " ---here5";
+                            string fileRelativePath = "~/uploadFiles/" + newFileName;
+                            outputForNir += " ---here6 imageName=" + fileRelativePath;
+                            Uri fileFullPath = new Uri(baseuri, VirtualPathUtility.ToAbsolute(fileRelativePath));
+                            outputForNir += " ---here7" + fileFullPath.ToString();
+                            savedFilePath.Add(fileFullPath.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            outputForNir += " ---excption=" + ex.Message;
+                            string message = ex.Message;
+                        }
+                    }
+
+                    return Request.CreateResponse(HttpStatusCode.Created, "nirchen " + savedFilePath[0] + "!" + provider.FileData.Count + "!" + outputForNir + ":)");
+                });
+            return task;
+        }
+
+        private string CreateDateTimeWithValidChars()
+        {
+            return DateTime.Now.ToString().Replace('/', '_').Replace(':', '-').Replace(' ', '_');
+        }
+    }
+
+
+}
+
