53dd2cd816bde0ba58e03b2f718723fde7ef7d70
tal farkash <tal_farkash@walla.com>
2022-03-31 13:42:44 +0300

    update image in db

--- diabeasy-back/Extention/Images.cs
+++ diabeasy-back/Extention/Images.cs
@@ -14,7 +14,7 @@ namespace diabeasy_back
         static Logger logger = LogManager.GetCurrentClassLogger();
         public string CreateNewNameOrMakeItUniqe(string name)
         {
-      
+
             try
             {
                 switch (name)
@@ -51,69 +51,72 @@ namespace diabeasy_back
         }
         public string ImageFileExist(string Name, string rootPath)
         {
-            
+
             string[] names = Directory.GetFiles(rootPath);
-         
+
             foreach (var fileName in names)
             {
                 if (Path.GetFileNameWithoutExtension(fileName).IndexOf(Path.GetFileNameWithoutExtension(Name)) != -1)
                 {
-                   
+
                     return fileName;
                 }
             }
-           
+
             return null;
         }
         void saveImageNameInDB(string name)
         {
             try
             {
-                logger.Debug("start of function saveImageNameInDB name="+name);
+                logger.Debug("start of function saveImageNameInDB name=" + name);
 
                 int id = int.Parse(string.Concat(name.ToArray().Reverse().TakeWhile(char.IsNumber).Reverse()));
-                logger.Debug("id="+id);
+                logger.Debug("id=" + id);
                 string pattern = @"\d+$";
-            string replacement = "";
-            Regex rgx = new Regex(pattern);
-            string result = rgx.Replace(name, replacement);
+                string replacement = "";
+                Regex rgx = new Regex(pattern);
+                string result = rgx.Replace(name, replacement);
                 logger.Debug("result=" + result);
-                switch (name)
-            {
-                case "profileDoctor":
-                    tblDoctor D= DB.tblDoctor.Where(x=>x.id==id).SingleOrDefault();
-                    if (D != null)
-                    {
-                        D.profileimage = name + ".jpg";
-                    }
+
+                switch (result)
+                {
+                    case "profileDoctor":
+                       
+                        tblDoctor D = DB.tblDoctor.Where(x => x.id == id).SingleOrDefault();
+                        if (D != null)
+                        {
+                            D.profileimage = name + ".jpg";
+                            logger.Debug("name=" + name + ".jpg");
+                        }
                         logger.Debug("D=" + D);
                         break;
 
-                case "profilePatient":
-                    tblPatients p = DB.tblPatients.Where(x => x.id == id).SingleOrDefault();
-                    if (p != null)
-                    {
-                        p.profileimage = name + ".jpg";
-                    }
-                    break;
-                case "Ingredient":
-                    Ingredients i = DB.Ingredients.Where(x => x.id == id).SingleOrDefault();
-                    if (i != null)
-                    {
-                        i.image = name + ".jpg";
-                    }
-                    break;
-                case "recipe":
-                    Recipes r = DB.Recipes.Where(x => x.id == id).SingleOrDefault();
-                    if (r != null)
-                    {
-                        r.image = name + ".jpg";
-                    }
-                    break;
-                default:
-                    break;
-            }
-            DB.SaveChanges();
+                    case "profilePatient":
+                        tblPatients p = DB.tblPatients.Where(x => x.id == id).SingleOrDefault();
+                        if (p != null)
+                        {
+                            p.profileimage = name + ".jpg";
+                        }
+                        break;
+                    case "Ingredient":
+                        Ingredients i = DB.Ingredients.Where(x => x.id == id).SingleOrDefault();
+                        if (i != null)
+                        {
+                            i.image = name + ".jpg";
+                        }
+                        break;
+                    case "recipe":
+                        Recipes r = DB.Recipes.Where(x => x.id == id).SingleOrDefault();
+                        if (r != null)
+                        {
+                            r.image = name + ".jpg";
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                DB.SaveChanges();
                 logger.Debug("saveChanges");
             }
             catch (Exception ex)
