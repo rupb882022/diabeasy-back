380d6b70fe2fae4b377c16534e895c271b0cda47
tal farkash <tal_farkash@walla.com>
2022-04-30 17:54:27 +0300

    break point

---
+++ WebApi/DTO/AteDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.DTO
+{
+    public class AteDto
+    {
+        public string unitName;
+        public Nullable<double> amount;
+        public Nullable<int> foodId;
+    }
+}
