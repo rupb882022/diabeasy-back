4f54d5c2090b8743c216f1c6d8cf3c37b20c764d
idanlavee <idanlavee10@gmail.com>
2022-05-10 10:55:13 +0300

    stam

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -434,7 +434,7 @@ namespace WebApi.Controllers
                 tblPatients p = DB.tblPatients.SingleOrDefault(x => x.id == id);
                 if (p != null)
                 {
-                    logger.Fatal(obj);
+                    //  DB.tblPrescriptions.Add(obj);
                     p.pushtoken = obj.pushtoken;
                     DB.SaveChanges();
                     return Created(new Uri(Request.RequestUri.AbsoluteUri), "OK");
